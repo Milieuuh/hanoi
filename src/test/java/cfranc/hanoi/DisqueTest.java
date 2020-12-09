478c074989b1c0094a432b0f154b33fdce414fb6
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-12-09 15:37:40 +0100

    test de la classe disque et tour

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -10,20 +10,117 @@ public class DisqueTest {
 	Disque dMedium = new Disque(2);
 	Disque dTall = new Disque(3);
 
+	/**
+	 * COMPARE
+	 */
 	@Test
 	public void testCompareTo() {
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
+
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+
+		assertEquals(-1,petit.compareTo(grand));
+		assertEquals(1,moyen.compareTo(petit));
+		assertEquals(1,grand.compareTo(moyen));
+		assertEquals(0,petit.compareTo(petit));
+		assertEquals(1,moyen.compareTo(petit));
+		assertEquals(0,moyen.compareTo(moyen));
+		assertEquals(-1,moyen.compareTo(grand));
 	}
 
+	/**
+	 * GIVEN -1
+	 * WHEN small et medium
+	 */
 	@Test
 	public void compareTo_SmallMedium_Negative(){
 		int expected = -1;
 		int actual = dSmall.compareTo(dMedium);
 		assertEquals(expected, actual);
 	}
+
+
+	/**
+	 * GIVEN -1
+	 * WHEN small et tall
+	 */
+	@Test
+	public void compareTo_SmallTall_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * GIVEN -1
+	 * WHEN small et tall
+	 */
+	@Test
+	public void compareTo_MediumTall_Negative(){
+		int expected = -1;
+		int actual = dMedium.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+
+
+	/**
+	 * GIVEN 1
+	 * WHEN small et medium
+	 */
+	@Test
+	public void compareTo_MediumSmall_Positive(){
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * GIVEN 1
+	 * WHEN tall et small
+	 */
+	@Test
+	public void compareTo_TallSmall_Negative(){
+		int expected = 1;
+		int actual = dTall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+
+	/**
+	 * GIVEN 1
+	 * WHEN tall et small
+	 */
+	@Test
+	public void compareTo_TallMedium_Negative(){
+		int expected = 1;
+		int actual = dTall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * GIVEN 0
+	 * WHEN medium et medium
+	 */
+	@Test
+	public void compareTo_MediumMedium_zero(){
+		int expected = 0;
+		int actual = dMedium.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+
+
+	/**
+	 * GIVEN 0
+	 * WHEN medium et medium
+	 */
+	@Test
+	public void compareTo_TallTall_zero(){
+		int expected = 0;
+		int actual = dTall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -6,9 +6,79 @@ import org.junit.Test;
 
 public class TourTest {
 
+	/**
+	 * GIVEN False
+	 * WHEN tourPleine
+	 */
 	@Test
-	public void test() {
+	public void testEmpiler_TourPleine_False() {
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+
+		Tour _TourPleine = new Tour(1);
+		_TourPleine.empiler(d2);
+
+		boolean expected = false ;
+		boolean actual = _TourPleine.empiler(d1);
+
+		assertEquals(expected, actual);
 
 	}
+
+	/**
+	 * GIVEN TRUE
+	 * WHEN TourNonVide avec disque de diamètre plus petit
+	 */
+	@Test
+	public void testEmpiler_TourNonVideDisquePlusPetit_True() {
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+
+		Tour _TourNonVide = new Tour(2);
+		_TourNonVide.empiler(d2);
+
+		boolean expected = true ;
+		boolean actual = _TourNonVide.empiler(d1);
+
+		assertEquals(expected, actual);
+
+	}
+	/**
+	 * GIVEN false
+	 * WHEN ajout sur tour non vide un diamètre plus grand que celui en dessous
+	 */
+	@Test
+	public void testEmpiler_TourNonVideDisquePlusGrand_False() {
+		Disque d1 = new Disque(2);
+		Disque d2 = new Disque(1);
+
+		Tour _TourNonVide = new Tour(1);
+		_TourNonVide.empiler(d2);
+		boolean expected = false ;
+		boolean actual = _TourNonVide.empiler(d1);
+
+		assertEquals(expected, actual);
+
+	}
+
+
+	/**
+	 * GIVEN TRUE
+	 * WHEN Tour vide
+	 */
+	@Test
+	public void testEmpiler_TourVide_True() {
+		Disque d1 = new Disque(1);
+
+		Tour _TourNonVide = new Tour(1);
+
+		boolean expected = true ;
+		boolean actual = _TourNonVide.empiler(d1);
+
+		assertEquals(expected, actual);
+
+	}
+
+
 
 }
